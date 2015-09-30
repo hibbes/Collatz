beb712bb9b689a08dd76984bf7d96c1d53818969
marek <marek@192.168.12.235>
2015-09-30 21:38:21 +0200

    Schleife für viele Folgen

--- Collatz/src/Collatz.java
+++ Collatz/src/Collatz.java
@@ -2,21 +2,27 @@
 public class Collatz {
 
 	public static void main(String[] args) {
-	
- 	 long startzahl = 435;
-	
-	 while(startzahl >1){		
-			if(startzahl%2==0){
-				startzahl=startzahl/2;
+	 long startzahl = 1;
+ 	 
+	while(startzahl<=1000){
+	 long zahl = startzahl;
+	 
+	 if(startzahl>1){System.out.print(" für "+ (startzahl)+": "); }
+	 while(zahl >1){		
+			if(zahl%2==0){
+				zahl=zahl/2;
 			    }
 			
-			else{startzahl=(startzahl*3)+1;}
+			else{zahl=(zahl*3)+1;}
 			
-			if(startzahl>1){System.out.print(startzahl+", ");}
-			else{System.out.print(startzahl);}
+		
+			if(zahl>1){System.out.print(zahl+", ");}
+			else
+			if (zahl<10){System.out.println(zahl);}
 		 
 	 }
-
-	}
 	
+    startzahl++;
+	}
+	}
 }
